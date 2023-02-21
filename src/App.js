379a90218e0b6b746cd55eb2a846504e6b3a87c5
trublesomeban <boronski.antoni@lo2katowice.edu.pl>
2023-02-21 20:22:06 +0100

    Refactor and imrpove

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <nav className='nav'>
-        <a className='nav-item' href="/#">Burze</a>
-        <a className='nav-item' href="/#">Powodzie</a>
-        <a className='nav-item' href="/#">Cyklony</a>
-        <a className='nav-item' href="/#">Tornada</a>
-        <a className='nav-item' href="/#">Śnieżyce</a>
-      </nav>
+      <Navbar/>
       <div className='info'>Ta strona jest w budowie. Jeśli coś zepsujesz, zgłoś to na <a href='https://github.com/trublesomeban/ezp-strona'>stronie projektu</a>.</div>
     </div>
   );
 }
 
+const Navbar = () => {
+ return <nav className='nav'>
+        <a className='nav-item' href="/#">Burze</a>
+        <a className='nav-item' href="/#">Powodzie</a>
+        <a className='nav-item' href="/#">Cyklony</a>
+        <a className='nav-item' href="/#">Tornada</a>
+        <a className='nav-item' href="/#">Śnieżyce</a>
+  </nav>
+}
 
 export default App;
