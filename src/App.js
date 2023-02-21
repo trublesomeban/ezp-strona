3cb54dbcf3aa19477b24c420526d9cc34d288cae
trublesomeban <boronski.antoni@lo2katowice.edu.pl>
2023-02-21 19:44:46 +0100

    Improve navbar

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <a className='nav-item' href="/#">Tornada</a>
         <a className='nav-item' href="/#">Śnieżyce</a>
       </nav>
+      <div className='info'>Ta strona jest w budowie. Jeśli coś zepsujesz, zgłoś to na <a href='https://github.com/trublesomeban/ezp-strona'>stronie projektu</a>.</div>
     </div>
   );
 }
