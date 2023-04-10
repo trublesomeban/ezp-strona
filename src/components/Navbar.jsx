47d4e209a42443b85efab80c82cd9e8e5abac060
trublesomeban <boronski.antoni@lo2katowice.edu.pl>
2023-04-10 15:54:48 +0200

    Restyle and add navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+// ICONS
+import * as FaIcons from "react-icons/fa"; //Now i get access to all the icons
+import * as AiIcons from "react-icons/ai";
+
+import { IconContext } from "react-icons";
+
+import { Link } from "react-router-dom";
+
+import { SidebarData } from "./SidebarData";
+
+import "./Navbar.css";
+
+export default function Navbar() {
+  return (
+    <>  
+      <IconContext.Provider value={{ color: "black" }}>
+        <nav className={"nav-menu active"}>
+          <ul className="nav-menu-items">
+            {SidebarData.map((item, index) => {
+              return (
+                <li key={index} className={item.cName}>
+                  <Link to={item.path}>
+                    {item.icon}
+                    <span>{item.title}</span>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </IconContext.Provider>
+    </>
+  );
+}
---
+++ src/components/SidebarData.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import * as AiIcons from "react-icons/ai";
+import * as IoIcons from "react-icons/io";
+
+export const SidebarData = [
+  {
+    title: "Home",
+    path: "/",
+    icon: <AiIcons.AiFillHome color="#317646"/>,
+    cName: "nav-text"
+  },
+  {
+    title: "Sources",
+    path: "/sources",
+    icon: <IoIcons.IoIosPaper color="#317646"/>,
+    cName: "nav-text"
+  },
+];
